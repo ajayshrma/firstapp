709d50abee5472d77025251d33fbc9ee2d16410a
ajayshrma <ajaysharma27600@gmail.com>
2024-01-21 11:45:45 +0530

    db connection done

--- api/server.js
+++ api/server.js
@@ -1,13 +1,35 @@
-const express = require("express"); //requires (imports) the
-//express package
+import express from "express"
+import connectDB from "./db/db_connection.js"; 
+import cors from 'cors';
 const PORT = process.env.PORT || 5000; //see below
 const server = express();
 
-server.use(express.json());
-server.use(express.urlencoded({ extended: true }));
+server.use(cors());
 
-server.get("/api/hello", (req, res) => {
-  res.status(200).send("Hello vijay");
-});
 
-server.listen(PORT, () => console.log(`listening on port ${PORT}`));
+
+connectDB()
+    .then(() => {
+      server.use(express.json());
+      server.use(express.urlencoded({ extended: true }));
+        // // Add the POST route for form submissions
+        // app.post('/api/submit-form', submissionController.submitForm);
+       
+        server.get("/api/hello", (req, res) => {
+          res.status(200).send("Hello vijay");
+          console.log(res);
+        });
+
+        server.listen(PORT, () => console.log(`listening on port ${PORT}`));
+    })
+    .catch((error) => {
+        console.error('MONGODB connection failed!!!', error);
+    });
+
+
+ 
+
+
+
+
+
